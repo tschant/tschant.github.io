cc66d7673ad69cf96467a5afbd6a137634319866
blakehefley <blakehefley@gmail.com>
2014-10-05 14:36:53 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -1,10 +1,12 @@
 <? 
 $conn = mysql_connect('mysql.eecs.ku.edu', 'tschante', 'monkey08')
 	or die('Could not connect: ' . mysql_error());
+$conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'PS4thewin');
+	or die('Could not connect: ' . mysql_error());
 //echo 'Connected successfully';
-mysql_select_db('tschante') or die('Could not select database');
+mysql_select_db('chefley') or die('Could not select database');
 
-$result = mysql_query("SELECT * FROM CRUISE",$conn);
+$result = mysql_query("SELECT * FROM CRUISE",$conn2);
 
 while($r = mysql_fetch_row($result)){
 	print_r($r);
