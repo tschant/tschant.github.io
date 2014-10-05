17dd57db15a194d1066a194554081ff840a28667
blakehefley <blakehefley@gmail.com>
2014-10-05 17:51:10 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -7,15 +7,24 @@ $conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'Ug67Ktg8')
 mysql_select_db('chefley') or die('Could not select database');
 
 $result = mysql_query("SELECT * FROM Location",$conn2);
-
 $r = mysql_fetch_row($result);
+$user = mysql_query("SELECT * FROM User",$conn2);
+$u = mysql_fetch_row($user);
+
 $elementsperrow = mysql_num_fields($result) - 3;
 $elementsperrow = sqrt($elementsperrow);
 $percentelement = 100/ $elementsperrow;
+
 for($i = 3; $i<mysql_num_fields ($result); $i++){
+	if($u[1] == $i){
+		Echo "<div style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\"><div style=\"background-color:white; width:50%; height:50%;margin:auto;\">&nbsp;</div></div>";
+	}
 Echo "<div style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\">&nbsp;</div>";
 }
+
 ?>
+
+
 <style>
 body{
 	margin:0;
