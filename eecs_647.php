789197cda71bbd11a7c73d0daca7df94a64ce9bb
blakehefley <blakehefley@gmail.com>
2014-10-05 18:08:54 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -29,7 +29,7 @@ document.onkeypress = function(evt) {
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
     var charStr = String.fromCharCode(charCode);
-    alert(charStr);
+    alert(charCode);
 };
 </script>
 <style>
