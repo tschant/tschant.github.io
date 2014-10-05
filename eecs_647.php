642473a05adf8ebab2bf00f630a564608c0071ad
blakehefley <blakehefley@gmail.com>
2014-10-05 16:05:12 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -6,7 +6,7 @@ $conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'Ug67Ktg8')
 //echo 'Connected successfully';
 mysql_select_db('chefley') or die('Could not select database');
 
-$result = mysql_query("SELECT Image* FROM Location",$conn2);
+$result = mysql_query("SELECT * FROM Location",$conn2);
 
 $r = mysql_fetch_row($result);
 
