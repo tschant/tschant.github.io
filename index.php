894c289442b23a2d168947d52ca1b42180a863f6
T <dawnmonkey08@gmail.com>
2014-10-05 14:09:17 -0500

    Commiting

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
+<!DOCTYPE html>
 <html>
-<? 
-$conn = mysql_connect('mysql.eecs.ku.edu', 'tschante', 'monkey08')
-	or die('Could not connect: ' . mysql_error());
-//echo 'Connected successfully';
-mysql_select_db('tschante') or die('Could not select database');
-
-$result = mysql_query("SELECT * FROM CRUISE",$conn);
-
-while($r = mysql_fetch_row($result)){
-	print_r($r);
-	print_r( "<br/>");
-}
-?>
+<body>
+<?php include 'eecs_647.php'; ?>
+</body>
 </html>
