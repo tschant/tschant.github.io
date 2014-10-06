facecfd6009c11e208a150e9c3148886538bc9ad
blakehefley <blakehefley@gmail.com>
2014-10-06 12:59:41 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -29,6 +29,7 @@ for($i = 3; $i<mysql_num_fields ($result); $i++){
 var numEnemies = <? echo $r[0];?>;
 var myLoc = <? echo $u[1]; ?>;
 document.onload = function(){
+	alert("0");
 	var divs = document.getElementsByTagName("div");
 	var newhtml = "<div class = \"enemy\">&nbsp;</div>";
 	alert("1");
