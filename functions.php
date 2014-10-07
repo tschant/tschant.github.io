5c2b4dbf53e02c6735ec68d679a54fe12c20ff38
tschant <dawnmonkey08@gmail.com>
2014-10-06 22:06:20 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?
+if (isset($_POST['callFunc1'])) {
+	echo func1($_POST['callFunc1']);
+}
+
+function func1($data){
+	$conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'Ug67Ktg8')
+		or die('Could not connect: ' . mysql_error());
+	//echo 'Connected successfully';
+	mysql_select_db('chefley') or die('Could not select database');
+	$query = "UPDATE User Set Loc = ".$data." WHERE Username LIKE 'BlakeHefley'";
+	$user = mysql_query($query,$conn2);
+}
+
+?>
