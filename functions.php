a8c3d48cc7f6fd8488006a681d8e86773af573f2
tschant <dawnmonkey08@gmail.com>
2014-10-06 22:46:04 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?
 session_start();
+//HOW DO YOU WORK!!!
 $username = $_SESSION["username"];
 if (isset($_POST['callFunc1'])) {
 	echo func1($_POST['callFunc1']);
@@ -10,6 +11,7 @@ function func1($data){
 		or die('Could not connect: ' . mysql_error());
 	//echo 'Connected successfully';
 	mysql_select_db('chefley') or die('Could not select database');
+	$username = "BlakeHefley"; //Remove once session is working
 	$query = "UPDATE User Set Loc = ".$data." WHERE Username LIKE '".$username."'";
 	mysql_query($query,$conn2);
 	return array("success"=>true, "message"=>"Updated user: ".$username);
