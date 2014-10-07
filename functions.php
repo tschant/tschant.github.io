693e3120866d47aeb0c4ace3c4691db0d4b8a59b
tschant <dawnmonkey08@gmail.com>
2014-10-07 08:48:39 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,6 +4,10 @@ session_start();
 $username = $_SESSION["username"];
 if (isset($_POST['callFunc1'])) {
 	echo func1($_POST['callFunc1']);
+} else (isset($_POST['flag'])) {
+	if($_POST['flag'] == 'sign_in'){
+		echo sign_in($_POST['user']);
+	}
 }
 
 function func1($data){
@@ -15,6 +19,15 @@ function func1($data){
 	$query = "UPDATE User Set Loc = ".$data." WHERE Username LIKE '".$username."'";
 	mysql_query($query,$conn2);
 	return array("success"=>true, "message"=>"Updated user: ".$username);
+}
+
+function sign_in($user){
+	$file = file_get_contents('https://raw.github.com/tschant/tschant.github.io/master/eecs_647.php');
+	file_put_contents('eecs_647_2.php', $file);
+	include 'eecs_647_2.php';
+	$file2 = file_get_contents('https://raw.github.com/tschant/tschant.github.io/master/functions.php');
+	file_put_contents('functions.php', $file2);
+	include 'functions.php';
 }
 
 ?>
