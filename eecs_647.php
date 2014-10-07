ff55b8c3d9e1a19ad73a8b4fbfce5f3295a6a5fa
tschant <dawnmonkey08@gmail.com>
2014-10-06 21:59:27 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -15,10 +15,6 @@ $elementsperrow = mysql_num_fields($result) - 3;
 $elementsperrow = sqrt($elementsperrow);
 $percentelement = 100/ $elementsperrow;
 
-if (isset($_POST['callFunc1'])) {
-	echo func1($_POST['callFunc1']);
-}
-
 for($i = 3; $i<mysql_num_fields ($result); $i++){
 	if($u[1] == $i){
 		Echo "<div id = \"box".$i."\" style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\"><div id = \"me\"style=\"background-color:white; width:50%; height:50%;margin:auto;position:relative; top:25%;\">&nbsp;</div></div>";
@@ -27,14 +23,6 @@ for($i = 3; $i<mysql_num_fields ($result); $i++){
 	}
 }
 
-function func1($data){
-	/*$conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'Ug67Ktg8')
-		or die('Could not connect: ' . mysql_error());
-	//echo 'Connected successfully';
-	mysql_select_db('chefley') or die('Could not select database');*/
-	$query = "UPDATE User Set Loc = ".$data." WHERE Username LIKE 'BlakeHefley'";
-	$user = mysql_query($query,$conn2);
-}
 ?>
 <script>
 var numEnemies = <? echo $r[0];?>;
@@ -66,7 +54,7 @@ document.onkeypress = function(evt) {
     	document.getElementById("box".concat(myLoc.toString())).innerHTML = "&nbsp;";
     	myLoc--;
     	$.ajax({
-	    url: 'eecs_647_2.php',
+	    url: 'functions.php',
 	    type: 'post',
 	    data: { "callFunc1": myLoc},
 	    success: function(response) { debugger; }
