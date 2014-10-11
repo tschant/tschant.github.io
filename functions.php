11a4e7274fa6b12cdc112f85b04c41ca50919443
blakehefley <blakehefley@gmail.com>
2014-10-11 10:20:47 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -56,9 +56,9 @@ function leaveR($Loc, $Locid){
 	$percentelement = 100/ $elementsperrow;
 	for($i = 3; $i<mysql_num_fields ($result); $i++){
 		if($Loc == $i){
-			$code = $code."<div id = \"box".$i."\" style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\"><div id = \"me\"style=\"background-color:white; width:50%; height:50%;margin:auto;position:relative; top:25%;\">&nbsp;</div></div>";
+			$code = $code."<div class=\"box\" id = \"box".$i."\" style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\"><div id = \"me\"style=\"background-color:white; width:50%; height:50%;margin:auto;position:relative; top:25%;\">&nbsp;</div></div>";
 		}else{
-			$code = $code."<div id = \"box".$i."\" style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\">&nbsp;</div>";
+			$code = $code."<div class=\"box\" id = \"box".$i."\" style=\"background-color: ".$r[$i]."; width: ".$percentelement."%; float: left; height: ".$percentelement."%;\">&nbsp;</div>";
 		}
 	}
 	return $code;
