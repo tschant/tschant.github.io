834f204d2e32b2e1161445812533cc03a036543b
tschant <dawnmonkey08@gmail.com>
2014-10-12 16:42:09 -0500

    Update eecs_647.php

--- eecs_647.php
+++ eecs_647.php
@@ -1,9 +1,9 @@
 <?
 $conn = mysql_connect('mysql.eecs.ku.edu', 'tschante', 'monkey08')
 	or die('Could not connect: ' . mysql_error());
-$conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'Ug67Ktg8')
-	or die('Could not connect: ' . mysql_error());
 $conn3 = mysql_connect('mysql.eecs.ku.edu', 'jlipscom', 'Uo16uI37')
+	or die('Could not connect: ' . mysql_error());
+$conn2 = mysql_connect('mysql.eecs.ku.edu', 'chefley', 'Ug67Ktg8')
 	or die('Could not connect: ' . mysql_error());
 //echo 'Connected successfully';
 mysql_select_db('chefley') or die('Could not select database');
