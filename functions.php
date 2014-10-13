baef274d8db2b084528e4e7b268807a2e2612de7
blakehefley <blakehefley@gmail.com>
2014-10-13 17:57:13 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -33,12 +33,8 @@ function func1($data){
 	$query = "UPDATE User Set Loc = ".$data." WHERE Username LIKE '".$username."'";
 	mysql_query($query,$conn2);
 	$names = mysql_query("Select * From ImageSet", $conn2);
-	while ($row = $names->fetchArray()){
-		for($i = 0; $i < $results->numColumns(); $i++){
-			if($row[$i] == "blue"){
-				mysql_query("UPDATE ImageSet Set ".columnName($i)." = 'http://i38.tinypic.com/25ul6hg.jpg'", $conn2);
-			}	
-		}
+	for($i = 0; $i < $results->numColumns(); $i++){
+		mysql_query("UPDATE ImageSet Set ".$results->columnName($i)." = 'http://i38.tinypic.com/25ul6hg.jpg' WHERE", $conn2);
 	}
 	return array("success"=>true, "message"=>"Updated user: ".$username);
 }
